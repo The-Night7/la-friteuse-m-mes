737c2dc95cb7fd5262312d45865fc7422e89c58d
The-Night7 <myriam.bensaid21@gmail.com>
2026-03-29 20:06:59 +0200

    dertrytuhij

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './App.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Look up the root container and mount the React application.  This entry
+// point assumes that your host HTML page contains a <div id="root"></div>.
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
