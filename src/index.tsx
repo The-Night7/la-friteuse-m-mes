c249fad9f129940c1d9a983a1f6d543bc575d2d2
The-Night7 <myriam.bensaid21@gmail.com>
2026-03-29 20:30:02 +0200

    ERTCRTYIUOI

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx'; // On importe le composant qu'on vient de créer
-import './index.css';        // Votre CSS global (facultatif)
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css'; // ou './App.css' selon comment tu l'as nommé
 
-// Création de la racine React et rendu du composant App
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// On cherche la balise <div id="root"></div> dans le index.html
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  // On injecte notre application dedans !
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
